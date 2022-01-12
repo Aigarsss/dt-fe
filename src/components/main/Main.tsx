8bfc2a256ff39235594657cbaabaa83df2af6c42
Aigars <aigars.uplejs@gmail.com>
2022-01-12 18:31:50 +0200

    Fix formatting

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -11,7 +11,6 @@ const Main: React.FC = () => {
     const { data, darkMode } = useAppContext();
 
     console.log(data);
-    
 
     return (
         <div className={classNames(styles.root, { [styles.darkMode]: darkMode })}>
@@ -19,19 +18,18 @@ const Main: React.FC = () => {
                 <Search />
             </div>
             <div className={styles.cardsContainer}>
-                {!data && <div>...Loading</div>}
+                {!data && <div>Loading...</div>}
                 {data &&
                     data.map((item) => {
                         return <Card key={item.id} item={item} />;
-                    })
-                }
+                    })}
             </div>
-            {data && data.length === 0 && 
+            {data && data.length === 0 && (
                 <div className={styles.noResults}>
                     <img src={gatis} alt="gatis" />
                     <p>No results</p>
                 </div>
-                }
+            )}
             <div className={styles.paginationContainer}>
                 <Pagination />
             </div>
