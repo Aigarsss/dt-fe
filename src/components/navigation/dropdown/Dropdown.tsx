bb98818f5878169c882e413c822ed75c4283f558
Aigars <aigars.uplejs@gmail.com>
2022-01-12 16:27:16 +0200

    Update pagination logic

--- src/components/navigation/dropdown/Dropdown.tsx
+++ src/components/navigation/dropdown/Dropdown.tsx
@@ -6,27 +6,27 @@ const Dropdown: React.FC = () => {
     const { setPageSize } = useAppContext();
     return (
         <div className={styles.root}>
-                <select
-                    name="pageSizes"
-                    defaultValue="12"
-                    id="pageSizes"
-                    className={styles.picker}
-                    onChange={(e) => setPageSize(Number(e.currentTarget.value))}
-                >
-                    <option value="6">6</option>
-                    <option value="12">12</option>
-                    <option value="18">18</option>
-                    <option value="24">24</option>
-                    <option value="30">30</option>
-                    <option value="36">36</option>
-                    <option value="42">42</option>
-                    <option value="48">48</option>
-                </select>
-                <span className={styles.customArrow}>
-                    <ArrowDown />
-                </span>
+            <select
+                name="pageSizes"
+                defaultValue="12"
+                id="pageSizes"
+                className={styles.picker}
+                onChange={(e) => setPageSize(Number(e.currentTarget.value))}
+            >
+                <option value="6">6</option>
+                <option value="12">12</option>
+                <option value="18">18</option>
+                <option value="24">24</option>
+                <option value="30">30</option>
+                <option value="36">36</option>
+                <option value="42">42</option>
+                <option value="48">48</option>
+            </select>
+            <span className={styles.customArrow}>
+                <ArrowDown />
+            </span>
         </div>
-    )
-}
+    );
+};
 
 export default Dropdown;
--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -9,38 +9,55 @@ const Pagination: React.FC = () => {
 
     return (
         <div className={styles.root}>
-            <span className={classNames(styles.chevron,{[styles.chevronDisabled]: currentPage === 1})} onClick={() => {
-                if (currentPage > 1) {
-                    setCurrentPage(prevPage => prevPage - 1)
-                }
-            } }>
+            {/* Left arrow */}
+            <span
+                className={classNames(styles.chevron, { [styles.chevronDisabled]: currentPage === 1 })}
+                onClick={() => {
+                    if (currentPage > 1) {
+                        setCurrentPage((prevPage) => prevPage - 1);
+                    }
+                }}
+            >
                 <Left />
             </span>
-            <span className={styles.number}>
-                ...
+            {/* First page */}
+            <span className={styles.number} onClick={() => setCurrentPage(1)}>
+                1
             </span>
+            {/* Conditional ... */}
+            {![1, 2, 3].includes(currentPage) && <span className={styles.number}>...</span>}
+            {/* Actual pages, Current +/- 2 */}
             {pageCount &&
                 pageCount
-                    .filter((page) => page === 1 || page === pageCount[pageCount.length - 1] || (page <= currentPage + 2 && page >= currentPage - 2))
+                    .filter((page) => page !== 1 && page !== pageCount[pageCount.length - 1] && page <= currentPage + 2 && page >= currentPage - 2)
                     .map((page) => {
                         return (
-                            <span 
-                                key={page} 
-                                onClick={() => setCurrentPage(page)} 
-                                className={classNames(styles.number, {[styles.selected]: page === currentPage})}
+                            <span
+                                key={page}
+                                onClick={() => setCurrentPage(page)}
+                                className={classNames(styles.number, { [styles.selected]: page === currentPage })}
                             >
                                 {page}
                             </span>
                         );
-            })}
-            <span className={styles.number}>
-                ...
+                    })}
+            {/* Conditional ... */}
+            {![pageCount[pageCount.length - 3], pageCount[pageCount.length - 2], pageCount[pageCount.length - 1]].includes(currentPage) && (
+                <span className={styles.number}>...</span>
+            )}
+            {/* Last page ... */}
+            <span className={styles.number} onClick={() => setCurrentPage(pageCount[pageCount.length - 1])}>
+                {pageCount[pageCount.length - 1]}
             </span>
-            <span className={classNames(styles.chevron,{[styles.chevronDisabled]: currentPage === pageCount[pageCount.length - 1]})} onClick={() => {
-                if (currentPage < pageCount[pageCount.length - 1]) {
-                    setCurrentPage(prevPage => prevPage + 1)
-                }
-            } }>
+            {/* Right arrow ... */}
+            <span
+                className={classNames(styles.chevron, { [styles.chevronDisabled]: currentPage === pageCount[pageCount.length - 1] })}
+                onClick={() => {
+                    if (currentPage < pageCount[pageCount.length - 1]) {
+                        setCurrentPage((prevPage) => prevPage + 1);
+                    }
+                }}
+            >
                 <Right />
             </span>
         </div>
